323563e30ddc52f712725ee4ea42330b7ec89124
SamuelWade <samuelrcwade@gmail.com>
2023-03-16 11:15:45 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+let returnObject = document.querySelector('body');
+
+function submitData(personName, personEmail) {
+    
+  
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: personName,
+            email: personEmail,
+        }
+        ),
+    }
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        returnObject.innerHTML = object.id;
+      })
+      .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        returnObject.innerHTML = error.message;
+      });
+      
+}
+
